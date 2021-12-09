8a0ddee5322d75c9360d22a544906f0743ff295f
Florian ABGRALL <florian.abgralldwwb@gmail.com>
2021-12-09 13:58:20 +0100

    add secret

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 def get_database():
-    CONNECTION_STRING = 'mongodb+srv://f-abgrall:admin@cluster0.2saqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    CONNECTION_STRING = 'mongodb+srv://username:password@cluster0.2saqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     client = MongoClient(CONNECTION_STRING)
     return client['Car']
 
