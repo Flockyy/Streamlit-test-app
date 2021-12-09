de13dfbb1b384cbf0ec4e1c02c272acfbfee8fdf
Florian ABGRALL <florian.abgralldwwb@gmail.com>
2021-12-09 14:31:48 +0100

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-import secrets
+from pymongo import MongoClient
 
 def get_database():
     CONNECTION_STRING = 'mongodb+srv://f-abgrall:admin@cluster0.2saqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-    client = secrets.pymongo.MongoClient(CONNECTION_STRING)
+    client = MongoClient(CONNECTION_STRING)
     return client['Car']
 
 db = get_database()
