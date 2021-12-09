fca1356c597aa7b3f7c7059b30f95927dda686dd
Florian ABGRALL <florian.abgralldwwb@gmail.com>
2021-12-09 16:58:45 +0100

    first selectbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,25 @@
 from pymongo import MongoClient
 import streamlit as st
 import pandas as pd
-
-def get_database():
-    CONNECTION_STRING = 'mongodb+srv://f-abgrall:admin@cluster0.2saqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-    client = MongoClient(CONNECTION_STRING)
-    return client['Car']
-
-db = get_database()
-carfeatures = db['CarFeatures']
-
-
-cars_constructor = [i for i in carfeatures.find({}, {'Make':1, '_id':0})]
+    
+CONNECTION_STRING = 'mongodb+srv://f-abgrall:admin@cluster0.2saqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+client = MongoClient(CONNECTION_STRING)
+db = client.Car
+cars = db.CarFeatures
+car_makers = cars.aggregate([{'$group':{"_id": {"Make": "$Make"}}}])
 
 st.write(""" # Car Features """)
+makers = []
+for i in car_makers:
+    makers.append(i['_id']['Make')
 
-st.write(cars_constructor)
+my_cont = st.container() 
+
+search_by_maker = st.sidebar.selectbox("Search by Maker", makers)
+search_by_model= st.sidebar.text_input("Search by Model")
+
+with my_cont:
+    if search_by_maker:
+        pass
+    if search_by_model:
+        pass
