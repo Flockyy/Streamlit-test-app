fc714e323cd7e3b7459260a37baf06a47d75b7ab
Flockyy <florian.abgrall750@gmail.com>
2021-12-10 13:41:43 +0100

    end

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,15 +22,44 @@ def search_by_maker_model():
         et {i['Engine Cylinders']} cylindres.
         Sa consommation sur autoroute est de {int(i['Highway L/100 km'])} L au 100 km 
         et de {int(i['City L/100 km'])} L au 100km en ville.""")
-        
+   
+maker_input = st.sidebar.selectbox("Search by Maker", car_makers)
+
+if maker_input:
+    car_models = cars.distinct('Model', {'Make': maker_input})  
+    model_input= st.sidebar.selectbox("Search by Model", car_models)
+    
+if maker_input and model_input:
+    search_by_maker_model()
+
+form = st.form(key='my_form')
+make = form.text_input('Constructeur')
+model = form.text_input('Modèle')
+year = form.text_input('Année')
+hp = form.text_input('Chevaux')
+cylinders = form.text_input('Cylindres')
+submit = form.form_submit_button('Submit')
+
 def add_car():
     cars.insert_one(
         {
             'Make': make,
             'Model': model,
             'Year': year,
+            'Engine Fuel Type' : '',
             'Engine HP': hp,
             'Engine Cylinders' : cylinders,
+            'Transmission Type': '',
+            'Driven_Wheels': '', 
+            'Number of Doors': 0,
+            'Market Category': '',
+            'Vehicle Size': '',
+            'Vehicle Style': '',
+            'Popularity': 0, 
+            'MSRP': 0,
+            'City L/100 km': 0,
+            'Highway L/100 km': 0,
+            'HP/Cylinders': 0
         }
     )
     created_car = cars.find_one(
@@ -43,23 +72,7 @@ def add_car():
         }
     )
     return created_car
-    
-maker_input = st.sidebar.selectbox("Search by Maker", car_makers)
 
-form = st.form(key='my_form')
-make = form.text_input('Constructeur')
-model = form.text_input('Modèle')
-year = form.text_input('Année')
-hp = form.text_input('Chevaux')
-cylinders = form.text_input('Cylindres')
-submit = form.form_submit_button('Submit')
-
-if maker_input:
-    car_models = cars.distinct('Model', {'Make': maker_input})  
-    model_input= st.sidebar.selectbox("Search by Model", car_models)
-    
-if maker_input and model_input:
-    search_by_maker_model()
 
 if submit:
     your_new_car = add_car()
