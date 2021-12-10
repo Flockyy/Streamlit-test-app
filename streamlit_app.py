3a1859d4ac3a14f72d062974e9f446348b3bbd3e
Flockyy <florian.abgrall750@gmail.com>
2021-12-10 14:51:55 +0100

    'end'

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,11 +17,15 @@ st.write(""" # Car Features """)
 def search_by_maker_model():
     researched_makers = cars.find({'Make': maker_input, 'Model': model_input}, {'_id': 0})
     for i in researched_makers:
-        st.write(f"""La {i['Make']} {i['Model']} {i['Vehicle Style']},
-        de {i['Year']} a {i['Engine HP']} chevaux 
-        et {i['Engine Cylinders']} cylindres.
-        Sa consommation sur autoroute est de {int(i['Highway L/100 km'])} L au 100 km 
-        et de {int(i['City L/100 km'])} L au 100km en ville.""")
+        try:
+            st.write(f"""La {i['Make']} {i['Model']} {i['Vehicle Style']},
+            de {i['Year']} a {i['Engine HP']} chevaux 
+            et {i['Engine Cylinders']} cylindres.
+            Sa consommation sur autoroute est de {int(i['Highway L/100 km'])} L au 100 km 
+            et de {int(i['City L/100 km'])} L au 100km en ville.""")
+        except:
+            st.write(f"""La {i['Make']} {i['Model']} {i['Vehicle Style']},
+            de {i['Year']} est une voiture électrique.""")
    
 maker_input = st.sidebar.selectbox("Search by Maker", car_makers)
 
@@ -35,9 +39,9 @@ if maker_input and model_input:
 form = st.form(key='my_form')
 make = form.text_input('Constructeur')
 model = form.text_input('Modèle')
-year = form.text_input('Année')
-hp = form.text_input('Chevaux')
-cylinders = form.text_input('Cylindres')
+year = form.number_input('Année', min_value=1900, max_value=2022, step=1)
+hp = form.number_input('Chevaux', min_value=1, max_value=1200, step=1)
+cylinders = form.number_input('Cylindres', min_value=1, max_value=16, step=1)
 submit = form.form_submit_button('Submit')
 
 def add_car():
@@ -55,11 +59,11 @@ def add_car():
             'Market Category': '',
             'Vehicle Size': '',
             'Vehicle Style': '',
-            'Popularity': 0, 
-            'MSRP': 0,
-            'City L/100 km': 0,
-            'Highway L/100 km': 0,
-            'HP/Cylinders': 0
+            'Popularity': 3000, 
+            'MSRP': 35000,
+            'City L/100 km': 10,
+            'Highway L/100 km': 14,
+            'HP/Cylinders': hp/cylinders
         }
     )
     created_car = cars.find_one(
@@ -76,4 +80,4 @@ def add_car():
 
 if submit:
     your_new_car = add_car()
-    st.write(f"The {your_new_car['Model']} from {your_new_car['Make']} have been added")
+    st.success(f"The {your_new_car['Model']} from {your_new_car['Make']} have been added")
